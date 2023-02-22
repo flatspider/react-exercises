fc6a81bc415babb37bcd5100ffeafbd8798eb94f
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-02-21 20:57:41 -0500

    Able to add new bookmarks. No tag functionality.

--- src/exercise-D/BookmarkList.js
+++ src/exercise-D/BookmarkList.js
@@ -22,21 +22,22 @@ function BookmarkList() {
   const [tagFilter, setTagFilter] = useState(null);
   const [bookmarkList, setBookmarkList] = useState(INITIAL_BOOKMARKS);
   const [newBookmarkUrl, setNewBookmarkUrl] = useState("");
+  const [newBookmarkTitle, setNewBookmarkTitle] = useState("");
+  const [newBookmarkTag, setNewBookmarkTag] = useState("");
 
   const handleSubmit = (submits) => {
     submits.preventDefault(); // Stops the button from refreshing the page.
 
     const newBookmark = {
       url: newBookmarkUrl,
-      title: "New Bookmark",
-      tag: "misc",
+      title: newBookmarkTitle,
+      tag: newBookmarkTag,
     };
 
-    // onSubmit is being passed in through the App.js html element
-    // Pull in the values of title, content, and author that the user typed.
-    // Call onSubmit property from below form.
     setBookmarkList([...bookmarkList, newBookmark]);
     setNewBookmarkUrl(""); // Clears the input box.
+    setNewBookmarkTitle(""); // Clears the input box.
+    setNewBookmarkTag(""); // Clears the input box.
   };
 
   return (
@@ -54,14 +55,37 @@ function BookmarkList() {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
+          name="URL"
+          className="form-control title w-75 mt-2 mx-auto"
+          id="bookmarkURL"
+          autoComplete="off"
+          placeholder="Enter website here..."
+          value={newBookmarkUrl}
+          onChange={(event) => setNewBookmarkUrl(event.target.value)} // This watches each change to the input and returns the current value
+        ></input>
+        <input
+          type="text"
           name="title"
           className="form-control title w-75 mt-2 mx-auto"
           id="blogTitle"
           autoComplete="off"
-          placeholder="Enter blog title here..."
-          value={newBookmarkUrl}
-          onChange={(event) => setNewBookmarkUrl(event.target.value)} // This watches each change to the input and returns the current value
+          placeholder="Enter description here..."
+          value={newBookmarkTitle}
+          onChange={(event) => setNewBookmarkTitle(event.target.value)} // This watches each change to the input and returns the current value
         ></input>
+        <input
+          type="text"
+          name="title"
+          className="form-control title w-75 mt-2 mx-auto"
+          id="blogTitle"
+          autoComplete="off"
+          placeholder="Enter tag here..."
+          value={newBookmarkTag}
+          onChange={(event) => setNewBookmarkTag(event.target.value)} // This watches each change to the input and returns the current value
+        ></input>
+        <button className="btn btn-secondary mt-2" type="submit">
+          Add bookmark!
+        </button>
       </form>
       {bookmarkList.map((bookmark, index) => (
         <Bookmark key={index} bookmark={bookmark} />
